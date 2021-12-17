0fbc7e4043271341524bf9ffd31c9adb8ce22d3b
Arkar <arkar.fr@gmail.com>
2021-12-17 22:27:03 +0630

    feat: Make nav fix to top on scroll and others
    Make nav fix to top on scroll and remove href from link in footer
    because they can't be connected with nav active feature and
    add show & off feature for header/nav too

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let navLinks = document.querySelector(".nav-links");
 let navLink = document.querySelectorAll(".nav-link");
 let navIcon = document.querySelector(".hamburger-icon");
 let navArr = Array.from(navLink);
+let windowHeight = window.innerHeight;
 
 navIcon.addEventListener("click",function() {
     navLinks.classList.toggle('show');
@@ -21,4 +22,19 @@ function removeActive(){
     navArr.forEach(el => {
         el.classList.remove('active');
     })
+}
+
+let header = document.querySelector('#header');
+
+window.onscroll = showRe;
+
+function showRe(e) {
+    if(window.pageYOffset > 200) {
+        header.classList.add('active')
+    } else if(window.pageYOffset < 200){
+        if(header.classList.contains('active')){
+            header.classList.remove('active');
+            removeActive();
+        }
+    }
 }
